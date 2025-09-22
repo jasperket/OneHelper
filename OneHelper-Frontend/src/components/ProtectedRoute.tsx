cf76d5a09be1466186232ec3dd6a0dfcff5fa3b7
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-22 17:22:32 +0800

    Add authcontext

---
+++ OneHelper-Frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "@/hooks/useAuth";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) return <div>Loading...</div>;
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
+}
