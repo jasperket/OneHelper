073c99a7c74f2058d256392af14525680fbfa141
TRAINEE - MATUGUINA <csmatuguina@n-pax.com>
2025-09-22 10:46:51 +0800

    Updated todo font and sleeplog

--- OneHelper-Frontend/src/pages/SleepTracker.tsx
+++ OneHelper-Frontend/src/pages/SleepTracker.tsx
@@ -1,24 +1,41 @@
 import AuthHeader from "@/components/layout/AuthHeader";
 import { DailySleepChart } from "@/components/sleep/DailySleepChart";
 import { WeeklySleepAve } from "@/components/sleep/WeeklySleepAve";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 export default function SleepTrackerPage() {
   const [isActive, setIsActive] = useState(false);
   return (
     <AuthHeader>
+      <h1 className="paytoneOne mb-7 text-5xl text-orange-400">
+        Sleep <span className="text-green-400">Tracker</span>
+      </h1>
       <div className="grid grid-cols-2 gap-24">
-        <Button
+        <button
           onClick={() => setIsActive(!isActive)}
-          className={`paytoneOne w-145 rounded-full py-10 text-[30px] transition-colors hover:bg-green-600 ${
-            isActive ? "bg-green-600 text-white" : "bg-green-700 text-white"
+          className={`paytoneOne relative h-[80px] w-145 overflow-hidden rounded-full py-5 text-[30px] text-white transition-colors duration-500 ease-in-out ${
+            isActive ? "bg-green-600" : "bg-amber-500"
           }`}
         >
-          {isActive ? "Start Tracking" : "Stop Tracking"}
-        </Button>
-        
-        <h1 className="rounded-full bg-green-600"/>
+          <span
+            className={`absolute inset-0 flex items-center justify-center transition-transform duration-500 ease-in-out ${
+              isActive ? "translate-y-0" : "-translate-y-full"
+            }`}
+          >
+            Start Tracking
+          </span>
+
+          <span
+            className={`absolute inset-0 flex items-center justify-center transition-transform duration-500 ease-in-out ${
+              isActive ? "translate-y-full" : "translate-y-0"
+            }`}
+          >
+            Stop Tracking
+          </span>
+        </button>
+
+        <h1 className="rounded-full bg-green-600" />
       </div>
       <div className="grid grid-cols-2 gap-24 py-10 text-gray-700">
         <DailySleepChart />
