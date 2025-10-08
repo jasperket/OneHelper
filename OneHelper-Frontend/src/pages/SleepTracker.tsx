0da0f5a73b59d16a42a9e20d10818e2e24849715
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-10-08 17:31:40 +0800

    feat: add and update sleep logs

--- OneHelper-Frontend/src/pages/SleepTracker.tsx
+++ OneHelper-Frontend/src/pages/SleepTracker.tsx
@@ -1,10 +1,59 @@
 import AuthHeader from "@/components/layout/AuthHeader";
 import { DailySleepChart } from "@/components/sleep/DailySleepChart";
 import { WeeklySleepAve } from "@/components/sleep/WeeklySleepAve";
-import { useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+import type { SleepRequest } from "@/models/sleepLog";
+import {
+  createSleepLog,
+  getActiveSleepLog,
+  updateSleepLog,
+} from "@/services/sleepClient";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export default function SleepTrackerPage() {
-  const [isActive, setIsActive] = useState(false);
+  const queryClient = useQueryClient();
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["activeSleepLog"],
+    queryFn: getActiveSleepLog,
+  });
+
+  const handleSleepLogBtn = () => {
+    if (data) {
+      stopSleepLog.mutate();
+    } else {
+      startSleepLog.mutate();
+    }
+  };
+
+  const handleStart = async () => {
+    const payload: SleepRequest = {
+      startTime: new Date().toISOString(),
+    };
+    await createSleepLog(payload);
+  };
+
+  const handleStop = async () => {
+    if (!data) return;
+    const payload: SleepRequest = {
+      startTime: new Date().toISOString(),
+      endTime: new Date().toISOString(),
+    };
+    await updateSleepLog(data?.id, payload);
+  };
+
+  const invalidateActiveSleepLog = async () => {
+    await queryClient.invalidateQueries({ queryKey: ["activeSleepLog"] });
+  };
+
+  const startSleepLog = useMutation({
+    mutationFn: handleStart,
+    onSuccess: invalidateActiveSleepLog,
+  });
+
+  const stopSleepLog = useMutation({
+    mutationFn: handleStop,
+    onSuccess: invalidateActiveSleepLog,
+  });
 
   return (
     <AuthHeader>
@@ -13,25 +62,37 @@ export default function SleepTrackerPage() {
       </h1>
       <div className="grid grid-cols-2 gap-24">
         <button
-          onClick={() => setIsActive(!isActive)}
           className={`paytoneOne relative h-[80px] w-145 overflow-hidden rounded-full py-5 text-[30px] text-white transition-colors duration-500 ease-in-out ${
-            isActive ? "bg-amber-500" : "bg-green-600"
+            data ? "bg-amber-500" : "bg-green-600"
           }`}
+          onClick={handleSleepLogBtn}
         >
           <span
             className={`absolute inset-0 flex items-center justify-center transition-transform duration-500 ease-in-out ${
-              isActive ? "translate-y-full" : "-translate-y-0"
+              data ? "translate-y-full" : "-translate-y-0"
             }`}
           >
-            Start Tracking
+            {isPending ? (
+              <>
+                <Spinner /> Loading
+              </>
+            ) : (
+              "Start Tracking"
+            )}
           </span>
 
           <span
             className={`absolute inset-0 flex items-center justify-center transition-transform duration-500 ease-in-out ${
-              isActive ? "translate-y-0" : "translate-y-full"
+              data ? "translate-y-0" : "translate-y-full"
             }`}
           >
-            Stop Tracking
+            {isPending ? (
+              <>
+                <Spinner /> Loading
+              </>
+            ) : (
+              "Stop Tracking"
+            )}
           </span>
         </button>
         <h1 className="rounded-full bg-green-600" />
